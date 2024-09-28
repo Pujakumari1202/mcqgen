f6ace23f811ac3b844383f580d21ec41d4618bd7
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-09-28 23:35:13 +0530

    updated1

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 #for install the local package
+from setuptools import find_packages,setup
+
+#method
+setup(
+    name='mcqgenerator',  #all parameters
+    version='0.0.1',
+    author='puja_kumari',
+    author_email='puja.kumari@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dorenv","PyPDF2"],
+    packages=find_packages()
+)
